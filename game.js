c30b0e4ec02773fbdb71bfaae3f7bb19b45e6f64
Justin Cole <19556622+Jcole429@users.noreply.github.com>
2023-03-17 20:04:07 -0400

    Refactor to playSound()

--- game.js
+++ game.js
@@ -1,12 +1,12 @@
 var pathToSounds = "sounds/";
 var userClickedPattern = [];
 
-function playButton(color) {
+function playSound(color) {
     var audio = new Audio(pathToSounds + color + ".mp3");
     audio.play();
 }
 
 $(".btn").on("click", function () {
     userClickedPattern.push(this.id);
-    playButton(this.id);
+    playSound(this.id);
 });
