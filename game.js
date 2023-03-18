f7a7e18e064b828ee2288d3d903be8f22cd77350
Justin Cole <19556622+Jcole429@users.noreply.github.com>
2023-03-17 20:10:07 -0400

    Animate clicking button

--- game.js
+++ game.js
@@ -6,7 +6,18 @@ function playSound(color) {
     audio.play();
 }
 
+function animatePress(currentColor) {
+    var pressedButton = $("#" + currentColor)
+
+    pressedButton.addClass("pressed");
+
+    setTimeout(function () {
+        pressedButton.removeClass("pressed");
+    }, 100);
+}
+
 $(".btn").on("click", function () {
     userClickedPattern.push(this.id);
     playSound(this.id);
+    animatePress(this.id);
 });
